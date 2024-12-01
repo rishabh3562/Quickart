f2a98a5426addbe449dadc676df408af69342d9d
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2024-12-01 17:52:59 +0530

    logout route

---
+++ src/pages/api/auth/logout.js
@@ -0,0 +1,31 @@
+import cookie from "cookie";
+import { serialize } from "cookie";
+// Logout user
+export default async function handler(req, res) {
+    if (req.method === "POST") {
+        try {
+            // Set cookies to expire immediately to logout the user
+            res.setHeader("Set-Cookie", [
+                serialize("accessToken", "", {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production", // Ensure secure cookies in production
+                    maxAge: 0, // Expire the cookie
+                    path: "/",
+                }),
+                serialize("refreshToken", "", {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production",
+                    maxAge: 0, // Expire the cookie
+                    path: "/",
+                }),
+            ]);
+
+            res.status(200).json({ message: "Logout successful" });
+        } catch (error) {
+            console.error('Error during logout:', error);
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    } else {
+        res.status(405).json({ message: "Method Not Allowed" });
+    }
+}
